e9cbe489dab2e1499ece11894f6e31baa3e7c38d
Donald van de Weyer <github.com@afaik.org>
2026-06-10 10:42:40 +0200

    feat: bump keyminder to 0.1.105

--- Casks/keyminder.rb
+++ Casks/keyminder.rb
@@ -1,8 +1,8 @@
 cask "keyminder" do
-  version "0.1.101"
-  sha256 "4e337e13c1539e23026d11672b9a9ea488e01792a856fbef5de2da57aef986a6"
+  version "0.1.105"
+  sha256 "6f569ce5a10fd1bb6aeae14b0c0f8e6c52fc2ff9ea99737ad9daf2b0dd439d4c"
 
-  url "https://keyminder.app/KeyMinder_#{version}.zip"
+  url "https://keyminder.app/KeyMinder_#{version}.dmg"
   name "KeyMinder"
   desc "Menu-bar app that shows keyboard shortcuts of the frontmost app"
   homepage "https://keyminder.app"
