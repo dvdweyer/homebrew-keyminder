accdead0b0f516835bc0165fea47fe14bb1ef677
Donald van de Weyer <github.com@afaik.org>
2026-06-10 17:23:34 +0200

    chore: bump KeyMinder cask to v1.0.110

--- Casks/keyminder.rb
+++ Casks/keyminder.rb
@@ -1,6 +1,6 @@
 cask "keyminder" do
-  version "1.0.108"
-  sha256 "4b1f03d0f3549477f55d5ce115fa772798d2756e410e7f5ab5015de318c88b53"
+  version "1.0.110"
+  sha256 "6fe403c2d62167b4821e7b3a59cf1de1af3ac92502b419e53de131134e2010d6"
 
   url "https://keyminder.app/KeyMinder_#{version}.dmg"
   name "KeyMinder"
