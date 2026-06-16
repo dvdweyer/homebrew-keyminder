71779e04e69707a3579c9132d4d0fe86fc4412f4
Donald van de Weyer <github.com@afaik.org>
2026-06-16 15:19:22 +0200

    feat: bump keyminder to 1.0.153

--- Casks/keyminder.rb
+++ Casks/keyminder.rb
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 cask "keyminder" do
-  version "1.0.130"
-  sha256 "2acd8faaca6b958d66dbdbf3c5fb2c92592e9a3e7249cab19e5f419dda6e773e"
+  version "1.0.153"
+  sha256 "7057d5e5973c743915724d361b7e4983c964dc6938e1f763d776b2ab549e197e"
 
   url "https://keyminder.app/KeyMinder_#{version}.dmg"
   name "KeyMinder"
